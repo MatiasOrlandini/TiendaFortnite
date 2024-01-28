855fa101ac9af0e2df855e8fd9f397ad188f4ad0
Matias <maty2327@hotmail.com>
2024-01-27 22:21:05 -0300

    add Vercel Web Analytics

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import FortniteShop from './FortniteShop';
 import reportWebVitals from './reportWebVitals';
+import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <FortniteShop />
+    <Analytics />
   </React.StrictMode>
 );
 
